892e0ca5ccfb6183deae49fa4bf391e1b90a8701
Trang Do <quynhtrangdo94@gmail.com>
2019-06-11 15:35:21 -0400

    Outlined the app

---
+++ student_db/src/studentapp/Student.java
@@ -0,0 +1,15 @@
+package studentapp;
+
+public class Student {
+  // Constructor: promt for student's name and year
+
+  // Generate ID
+
+  // Enroll in courses
+
+  // View Balance
+
+  // Pay tuition
+
+  // Show Status of student
+}
---
+++ student_db/src/studentapp/StudentDbApp.java
@@ -0,0 +1,7 @@
+public class StudentDbApp {
+  public static void main(String[] args) {
+    // Ask how many new users we want to add
+
+    // Create n number of students
+  }
+}
