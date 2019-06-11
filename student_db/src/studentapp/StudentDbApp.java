0581467f9f7241c22e5aeca9a0a7fda6fb38910d
Trang Do <quynhtrangdo94@gmail.com>
2019-06-11 15:59:09 -0400

    Set up private properties and constructor for Student

--- student_db/src/studentapp/StudentDbApp.java
+++ student_db/src/studentapp/StudentDbApp.java
@@ -1,6 +1,10 @@
+import studentapp.Student;
+
 public class StudentDbApp {
   public static void main(String[] args) {
+    Student st1 = new Student();
     // Ask how many new users we want to add
+
 
     // Create n number of students
   }
