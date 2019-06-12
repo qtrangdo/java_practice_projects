e0eb3e9c13566b7a6eb02018f9c2a1a0278af3ac
Trang Do <quynhtrangdo94@gmail.com>
2019-06-12 14:06:58 -0400

    Enroll multiple students

--- student_db/src/studentapp/StudentDbApp.java
+++ student_db/src/studentapp/StudentDbApp.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
+
 import studentapp.Student;
 
 public class StudentDbApp {
   public static void main(String[] args) {
-    Student st1 = new Student();
-    st1.enroll();
-    st1.payTuition();
-    System.out.println(st1.showInfo()); 
+
     // Ask how many new users we want to add
+    System.out.println("Enter number of students you want to enroll");
+    Scanner in = new Scanner(System.in);
+    int numOfStudents = in.nextInt();
+    Student[] students = new Student[numOfStudents];
 
 
     // Create n number of students
+    for (int n = 0; n < numOfStudents; n++) {
+      students[n] = new Student();
+      students[n].enroll();
+      students[n].payTuition();
+      System.out.println(students[n].showInfo()); 
+    }
   }
 }
