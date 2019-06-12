2d0b7d131c527082d3e0418be5a55bc1a6ceba7d
Trang Do <quynhtrangdo94@gmail.com>
2019-06-12 09:50:32 -0400

    Generate studentID with static properties

--- student_db/src/studentapp/Student.java
+++ student_db/src/studentapp/Student.java
@@ -6,10 +6,11 @@ public class Student {
   private String firstName;
   private String lastName;
   private int gradeYear;
-  private int studentID;
+  private String studentID;
   private String courses;
   private int tuitionBalance;
-  private int costOfCourse = 600;
+  private static int costOfCourse = 600;
+  private static int id = 1000;
 
   // Constructor: promt for student's name and year
   public Student() {
@@ -23,10 +24,19 @@ public class Student {
     System.out.println("Enter student level: \n1 - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior");
     this.gradeYear = in.nextInt();
 
-    System.out.println(firstName + lastName + gradeYear);
+    setStudentID();
+    
+    System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
   }
 
   // Generate ID
+  private void setStudentID() {
+    // increment id value everytime Student is created
+    id++;
+
+    // 5 digits: grade level + static id
+    this.studentID = this.gradeYear + "" + id;
+  }
 
   // Enroll in courses
 
