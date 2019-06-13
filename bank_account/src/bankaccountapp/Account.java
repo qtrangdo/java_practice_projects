37e6f549442aecf8058a8607914c29fc9215d418
Trang Do <quynhtrangdo94@gmail.com>
2019-06-13 16:12:05 -0400

    Generated account number

--- bank_account/src/bankaccountapp/Account.java
+++ bank_account/src/bankaccountapp/Account.java
@@ -1,12 +1,13 @@
 package bankaccountapp;
 
-public abstract class Account implements IBaseRate {
+public abstract class Account {
   // List common properties for checking and saving
   String name;
   String SSN;
   String accountNumber;
   double balance;
   double rate;
+  static int index = 10001;
 
 
   // Constructor to set base properties and initialize the account
@@ -15,8 +16,19 @@ public abstract class Account implements IBaseRate {
     this.SSN = SSN;
     this.balance = initDeposit;
     System.out.println(name + " " + SSN + " " + balance );
+
+    // Set account number
+    index++;
+    System.out.println(setAccountNumber()); 
   }
   
+  // Generate 11-digit account number: 1 or 2 depends on checking/saving, last 2 digit of SSN, unique 5-digit number, random 3-digit number
+  private String setAccountNumber() {
+    String lastTwoOfSSN = this.SSN.substring(this.SSN.length() - 2);
+    int uniqueID = index;
+    int randomNumber = (int) (Math.random() * Math.pow(10, 3));
+    return lastTwoOfSSN + uniqueID + randomNumber;
+  }
   // List common methods
 }
   
