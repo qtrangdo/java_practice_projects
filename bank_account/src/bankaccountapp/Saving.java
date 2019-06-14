a8ab2c6cbf136acd6056a98225db88f8059dfd3b
Trang Do <quynhtrangdo94@gmail.com>
2019-06-14 13:19:44 -0400

    Implemented safety deposit box

--- bank_account/src/bankaccountapp/Saving.java
+++ bank_account/src/bankaccountapp/Saving.java
@@ -9,11 +9,23 @@ public class Saving extends Account {
   public Saving(String name, String SSN, double initDeposit) {
     super(name, SSN, initDeposit);
     this.accountNumber = "2" + this.accountNumber;
+
+    setSafetyDepositBox();
+  }
+
+  private void setSafetyDepositBox() {
+    this.safetyDepositBoxID = (int) (Math.random() * Math.pow(10,3));
+    this.safetyDepositBoxKey = (int) (Math.random() * Math.pow(10,4));
   }
 
   // Methods specific to saving account
   public void showInfo() {
-    super.showInfo();
     System.out.println("ACCOUNT TYPE: SAVING");
+    super.showInfo();
+    System.out.println("FEATURES: " + 
+      "\n  Safety Deposit Box ID: " + safetyDepositBoxID + 
+      "\n  Safety Deposit Box Key: " + safetyDepositBoxKey
+    );
   }
+
 }
