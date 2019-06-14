7d2012a26474484217a560bf093b3c4a957fc9bf
Trang Do <quynhtrangdo94@gmail.com>
2019-06-14 13:25:50 -0400

    Implemented debit card

--- bank_account/src/bankaccountapp/Checking.java
+++ bank_account/src/bankaccountapp/Checking.java
@@ -9,11 +9,22 @@ public class Checking extends Account {
   public Checking(String name, String SSN, double initDeposit) {
     super(name, SSN, initDeposit);
     this.accountNumber = "1" + this.accountNumber;
+
+    setDebitCard();
+  }
+
+  private void setDebitCard() {
+    this.debitCardNumber = (int) (Math.random() * Math.pow(10,12));
+    this.debitCardPIN = (int) (Math.random() * Math.pow(10,4));
   }
 
   // Methods specific to checking account
   public void showInfo() {
-    super.showInfo();
     System.out.println("ACCOUNT TYPE: CHECKING");
+    super.showInfo();
+    System.out.println("FEATURES: " + 
+      "\n  Debit Card Number: " + debitCardNumber + 
+      "\n  Debit Card PIN: " + debitCardPIN
+    );
   }
 }
